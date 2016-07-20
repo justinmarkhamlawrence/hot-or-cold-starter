ce1866c017c461155e2a483369100a75acc05dee
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-07-20 16:35:15 -0400

    complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function(){
 	
+  var secretNum = parseInt(Math.random() * (100) + 1);
+  var counterNum = 0;
+  var userEntered = [];
+  
+  function newGame() { 
+    userNum = '';
+    counterNum = 0;
+    userEntered = [];
+    $('#feedback').text('Make your Guess!');
+    $('#count').text('0');
+    $('#guessList').text('');
+  }
+
+  $('.new').click(function() {
+    newGame();
+  }); 
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,68 +29,55 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-//Setting the secret number when page is loaded or reset
-
-var secretNum = parseInt(Math.random() * (100) + 1);
-// console.log('Secret Number is ' + secretNum)
-var counterNum = 0;
-var userEntered = [];
-
-//ON CLICK
-	// counter up
-	// assign uservalue (guess) to a variable
-	// assign the difference between uservalue and secretnum to a variable
-	// use if-then to provide a hotness rating that gets output to the result box
-	// once the user wins, display "you win" thing then ask for new game
-
     $('form').submit(function(event) {
         event.preventDefault();
         counterNum = counterNum + 1;
-        var userNum = $("#userGuess").val();
+        $('#count').text(counterNum);
+        var userNum = parseInt($("#userGuess").val());
         // console.log('Usernum is ' + userNum);
+        if (userNum > 100|| isNaN(userNum) || userNum === undefined) {
+          alert('Please enter an integer between 1 and 100');
+          return;
+        }
 
         for (var i = 0; i < userEntered.length; i++) {
-        	if (userEntered[i] == userNum){
-        	console.log('userNum is ' + userNum);
-        	console.log('userEntered Array in loop is ' + userEntered);
+        	if (userEntered[i] == userNum) {
         		alert('You already entered this number!');
         		return;
         	}
         }
 
-
         var diffNum = Math.abs(secretNum - userNum);
 
         if (diffNum >= 50){
-        	console.log('Ice cold');
+        	$('#feedback').text('Ice cold');
         }
         else if (diffNum < 50 && diffNum >= 30){
-        	console.log('Cold');
+        	$('#feedback').text('Cold');
         }
         else if (diffNum < 30 && diffNum >= 20){
-        	console.log('Warm');
+        	$('#feedback').text('Warm');
         }
         else if (diffNum < 20 && diffNum >= 10){
-        	console.log('Hot');
+        	$('#feedback').text('Hot');
         }        
         else if (diffNum < 10 && diffNum >= 1){
-        	console.log('Very hot!');
+        	$('#feedback').text('Very hot!');
         }               
         else {
-        	console.log('You win!');
+        	$('#feedback').text('You win! New Game?');
         }
 
         userEntered.push(userNum);
-        console.log('end of code ' + userEntered);
+ 
+        var guessAdd = '' + '<li>' + userNum + '</li>';
+        $(guessAdd).appendTo("#guessList");
+        $(this)[0].reset();
     })
 
-    //end
 });
 
 
-/* things to add
-- greater than 100
-- you entered this number already
-- add to list of things (add to ul as a li) ul "guessList" 
 
-*/
+
+
